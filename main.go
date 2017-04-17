142040a06904f8fb24c6adce06ea82a95d5167f9
Connor Murray <connormurray7@gmail.com>
2017-04-16 21:47:09 -0500

    setting up server and config

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s ", err))
+	}
+	addr := viper.GetString("address")
+	port := viper.GetString("port")
+
+	fmt.Printf("This is the addr %s and port %s", addr, port)
+}
