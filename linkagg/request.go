b141543e0ec5da57f3fe3748a62c39422c4b62bb
Connor Murray <connormurray7@gmail.com>
2017-04-24 21:41:59 -0500

    filling out request logic

--- linkagg/request.go
+++ linkagg/request.go
@@ -2,5 +2,23 @@ package linkagg
 
 //Requester makes a request to the outside APIs
 type Requester interface {
-	Request(req string)
+	Request(req string) string
+}
+
+type LinkAgg struct {
+	cache Cache
+}
+
+func (linkAgg *LinkAgg) Request(req string) string {
+	result := linkAgg.cache.Get(req)
+	if result != "" {
+		return result
+	}
+	resp := linkAgg.fetchExternalRequest(req)
+	linkAgg.cache.Set(req, resp)
+	return resp
+}
+
+func (linkAgg *LinkAgg) fetchExternalRequest(query string) string {
+
 }
