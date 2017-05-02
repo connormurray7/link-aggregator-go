058007aa8f2674043988f0e8194e5586e308b208
Connor Murray <connormurray7@gmail.com>
2017-05-01 20:41:44 -0500

    create requests

--- linkagg/request.go
+++ linkagg/request.go
@@ -9,11 +9,13 @@ type Requester interface {
 	Request(req string) string
 }
 
+//LinkAgg implements the requester interface and calls out to external APIs.
 type LinkAgg struct {
 	cache  *Cache
 	config *viper.Viper
 }
 
+//Request fetches all of the information from external APIs if not cached.
 func (linkAgg *LinkAgg) Request(req string) string {
 	result := linkAgg.cache.Get(req)
 	if result != "" {
@@ -25,5 +27,19 @@ func (linkAgg *LinkAgg) Request(req string) string {
 }
 
 func (linkAgg *LinkAgg) fetchExternalRequest(query string) string {
+	hnRequest := createHackerNewsRequest()
+	soRequest := createStackOverflowRequest()
+	ghRequest := createGithubRequest()
+}
+
+func createHackerNewsRequest() string {
+
+}
+
+func createStackOverflowRequest() string {
+
+}
+
+func createGithubRequest() string {
 
 }
