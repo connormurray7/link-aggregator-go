2f968ba17f13f5bc25a04c943f49dd22f4882230
Connor Murray <connormurray7@gmail.com>
2017-05-12 22:33:51 -0500

    refactoring to add server

--- linkagg/request.go
+++ linkagg/request.go
@@ -1,28 +1,14 @@
 package linkagg
 
 import (
-	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/spf13/viper"
 	"github.com/tidwall/gjson"
 )
-
-//Handler makes a request to the outside APIs
-type Handler interface {
-	Handle(w http.ResponseWriter, r *http.Request)
-}
-
-//LinkAgg implements the requester interface and calls out to external APIs.
-type LinkAgg struct {
-	cache  *Cache
-	config *viper.Viper
-	client *http.Client
-}
 
 //Message contains the information for every row in a response.
 type Message struct {
@@ -30,36 +16,11 @@ type Message struct {
 	link  string
 }
 
-//NewLinkAgg constructs a link agg given a config file.
-func NewLinkAgg(config *viper.Viper) LinkAgg {
-	var linkAgg LinkAgg
-
-	linkAgg.config = config
-	linkAgg.cache = NewLinkAggCache(config)
-	linkAgg.client = &http.Client{
-		Timeout: time.Second * 10,
-	}
-	return linkAgg
-}
-
-//Handle fetches all of the information from external APIs if not cached.
-func (linkAgg *LinkAgg) Handle(w http.ResponseWriter, r *http.Request) {
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
-	req := buf.String()
-	result := linkAgg.cache.Get(req)
-	if result == "" {
-		resp := linkAgg.fetchExternalRequest(req)
-		linkAgg.cache.Set(req, resp)
-		w.Write([]byte(resp))
-	}
-}
-
-func (linkAgg *LinkAgg) fetchExternalRequest(query string) string {
+func FetchExternalRequest(query string, config *viper.Viper, client *http.Client) string {
 	m := make(map[string][]Message)
-	m["Github"] = linkAgg.makeGithubRequest(query)
-	m["Hacker News"] = linkAgg.makeHackerNewsRequest(query)
-	m["Stack Overflow"] = linkAgg.makeStackOverflowRequest(query)
+	m["Github"] = makeGithubRequest(query, config, client)
+	m["Hacker News"] = makeHackerNewsRequest(query, config, client)
+	m["Stack Overflow"] = makeStackOverflowRequest(query, config, client)
 	result, err := json.Marshal(m)
 	if err != nil {
 		log.Print("Unable to encode response", err)
@@ -67,8 +28,8 @@ func (linkAgg *LinkAgg) fetchExternalRequest(query string) string {
 	return string(result)
 }
 
-func (linkAgg *LinkAgg) makeHackerNewsRequest(query string) []Message {
-	req, err := http.NewRequest("GET", linkAgg.config.GetString("HackerNews.url"), nil)
+func makeHackerNewsRequest(query string, config *viper.Viper, client *http.Client) []Message {
+	req, err := http.NewRequest("GET", config.GetString("HackerNews.url"), nil)
 	if err != nil {
 		log.Print(err)
 		return nil
@@ -78,12 +39,12 @@ func (linkAgg *LinkAgg) makeHackerNewsRequest(query string) []Message {
 	q.Set("tags", "story")
 	q.Set("hitsPerPage", "15")
 
-	json := linkAgg.makeRequest(req)
+	json := makeRequest(req, client)
 	return parseJSONResponse(json, "hits", "title", "url")
 }
 
-func (linkAgg *LinkAgg) makeStackOverflowRequest(query string) []Message {
-	req, err := http.NewRequest("GET", linkAgg.config.GetString("StackOverflow.url"), nil)
+func makeStackOverflowRequest(query string, config *viper.Viper, client *http.Client) []Message {
+	req, err := http.NewRequest("GET", config.GetString("StackOverflow.url"), nil)
 	if err != nil {
 		log.Print(err)
 		return nil
@@ -95,12 +56,12 @@ func (linkAgg *LinkAgg) makeStackOverflowRequest(query string) []Message {
 	q.Set("site", "stackoverflow")
 	q.Set("pagesize", "15")
 
-	json := linkAgg.makeRequest(req)
+	json := makeRequest(req, client)
 	return parseJSONResponse(json, "items", "title", "link")
 }
 
-func (linkAgg *LinkAgg) makeGithubRequest(query string) []Message {
-	req, err := http.NewRequest("GET", linkAgg.config.GetString("Github.url"), nil)
+func makeGithubRequest(query string, config *viper.Viper, client *http.Client) []Message {
+	req, err := http.NewRequest("GET", config.GetString("Github.url"), nil)
 	if err != nil {
 		log.Print(err)
 		return nil
@@ -110,12 +71,12 @@ func (linkAgg *LinkAgg) makeGithubRequest(query string) []Message {
 	q.Set("sort", "stars")
 	q.Set("per_page", "15")
 
-	json := linkAgg.makeRequest(req)
+	json := makeRequest(req, client)
 	return parseJSONResponse(json, "items", "name", "html_url")
 }
 
-func (linkAgg *LinkAgg) makeRequest(req *http.Request) string {
-	resp, err := linkAgg.client.Do(req)
+func makeRequest(req *http.Request, client *http.Client) string {
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Print("Unable to complete request", err)
 		return ""
@@ -125,7 +86,6 @@ func (linkAgg *LinkAgg) makeRequest(req *http.Request) string {
 		log.Print("Unable to read all bytes in response", resp, err)
 		return ""
 	}
-
 	return string(byteArr[:])
 }
 
---
+++ linkagg/server.go
@@ -0,0 +1,46 @@
+package linkagg
+
+import (
+	"bytes"
+	"net/http"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+//Handler makes a request to the outside APIs
+type Handler interface {
+	Handle(w http.ResponseWriter, r *http.Request)
+}
+
+//Server implements the requester interface and calls out to external APIs.
+type Server struct {
+	cache  *Cache
+	config *viper.Viper
+	client *http.Client
+}
+
+//NewServer constructs a new link agg server given a config file.
+func NewServer(config *viper.Viper) Server {
+	var server Server
+
+	server.config = config
+	server.cache = NewLinkAggCache(config)
+	server.client = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	return server
+}
+
+//Handle fetches all of the information from external APIs if not cached.
+func (server *Server) Handle(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	req := buf.String()
+	result := server.cache.Get(req)
+	if result == "" {
+		resp := linkAgg.fetchExternalRequest(req)
+		server.cache.Set(req, resp)
+		w.Write([]byte(resp))
+	}
+}
